31ba1c5d518a213ec558f351da197d20ca847344
inzaky <inzaky2000@yahoo.com>
2023-05-21 02:03:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './style.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <h3>Hello React !!<br/>สวัสดี React</h3>
+  
+  //ตัวอย่าง
+  // let r = 'React'
+  // let rn = 'React Native'
+  // const el = (
+  //       <div>
+  //         Create web & Mobile <br/>
+  //         witth {r} & {rn}
+  //       </div>
+  // )
+  // return el
+
+  // return เป็นตาราง
+  // return (
+	// 	<table border="1">
+	// 		<tr><th>Product</th><th>Price</th></tr>
+	// 		<tr><td>React</td><td>320</td></tr>
+	// 		<tr><td>React Native</td><td>340</td></tr>
+	// 	</table>
+	// )
+
+  const divStyle = {
+		color: 'red',
+		backgroundColor: 'powderblue',		
+		fontSize: 'larger',
+		padding: '3px',
+	}
+	return <div style={divStyle}>Hello React Navi</div>
+  // <table border="1">
+	//   <tr><th>Product</th><th>Price</th></tr>
+	//   <tr><td>React</td><td>320</td></tr>
+	//   <tr><td>React Native</td><td>340</td></tr>
+	// </table>
+
 }
 
 export default App;
